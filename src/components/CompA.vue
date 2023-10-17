50248de2b3f7682cc451a18376813e747f4b18bb
Matagi <31905238+XRayZen@users.noreply.github.com>
2023-10-17 22:01:02 +0900

    fianl commit

--- src/components/CompA.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-  import CompB from './CompB.vue';
-
-  const { message } = defineProps(['message']);
-</script>
-<template>
-  <h2>CompAコンポーネント</h2>
-  <CompB :message="message" />
-</template>
