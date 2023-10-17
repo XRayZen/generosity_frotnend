429acf8fcf7e4d19fba1366b316770129d5614d1
Matagi <31905238+XRayZen@users.noreply.github.com>
2023-10-18 00:42:50 +0900

    TEST PASS FetchPrefectures

--- src/components/FetchApi.vue
+++ src/components/FetchApi.vue
@@ -1,7 +1,43 @@
+<script>
+import fs from 'fs'
+import axios from 'axios'
 
+//他のコンポーネントで利用できる様にするためexport
+export default {
+  methods: {
+    async FetchApi(path, ACCESS_TOKEN) {
+      try {
+        const response = await axios.get(`https://opendata.resas-portal.go.jp/api/v1/${path}`, {
+          headers: { 'X-API-KEY': ACCESS_TOKEN }
+        })
+        console.log('api status:' + response.status)
+        return response
+      } catch (error) {
+        console.error(error.message)
+      }
+    },
+    // 都道府県一覧を取得する
+    async FetchPrefectures(ACCESS_TOKEN) {
+      try {
+        const response = await this.FetchApi('prefectures', ACCESS_TOKEN)
+        return response.data.result.map((val) => {
+          return {
+            id: val['prefCode'],
+            name: val['prefName'],
+            isChecked: false
+          }
+        })
+      } catch (error) {
+        console.error(error.message)
+      }
+    },
 
-
-
-
-
-
+    // プロジェクトルート直下APIKEY.mdの内容を読み込む
+    async ReadApiKey() {
+      const apiKeyPath = './APIKEY.md'
+      const apiKey = fs.readFileSync(apiKeyPath, 'utf8')
+      return apiKey
+    }
+  }
+}
+</script>
--- src/components/__tests__/FetchApiTest.spec.js
+++ src/components/__tests__/FetchApiTest.spec.js
@@ -1,45 +1,20 @@
 // import { mount } from '@vue/test-utils'
-import axios from 'axios'
+// import axios from 'axios'
 import { describe, it, expect } from 'vitest'
 import fs from 'fs'
 // import path from 'path'
+import FetchApi from '../FetchApi.vue'
 
 describe('FetchApiTest', () => {
   it('fetches data from API', async () => {
     // プロジェクトルート直下APIKEY.mdの内容を読み込む
     const apiKeyPath = './APIKEY.md'
     const apiKey = fs.readFileSync(apiKeyPath, 'utf8')
-    // axiosでAPIを叩く
-    const res = await FetchPrefectures(apiKey)
+    // インポートしているコンポーネントから関数を呼び出してaxiosでAPIを叩く
+    // Thanks Copilot!!
+    const res = await FetchApi.methods.FetchPrefectures(apiKey)
     console.log("api data:"+res[0].name)
     // 無事にデータが取得できているか確認
     expect(res.length).toBeGreaterThan(5)
   })
 })
-
-async function FetchPrefectures(ACCESS_TOKEN) {
-  try {
-    const response = await FetchApi('prefectures', ACCESS_TOKEN)
-    return response.data.result.map(val => {
-      return {
-        id: val["prefCode"],
-        name: val["prefName"],
-        isChecked: false
-      };
-    });
-  } catch (error) {
-    console.error(error.message)
-  }
-}
-
-async function FetchApi(path, ACCESS_TOKEN) {
-  try {
-    const response = await axios.get(`https://opendata.resas-portal.go.jp/api/v1/${path}`, {
-      headers: { 'X-API-KEY': ACCESS_TOKEN }
-    })
-    console.log("api status:"+response.status)
-    return response
-  } catch (error) {
-    console.error(error.message)
-  }
-}
