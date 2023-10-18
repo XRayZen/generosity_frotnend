e051efa4a8c4079838908ed08fc93d4086526acd
Matagi <31905238+XRayZen@users.noreply.github.com>
2023-10-18 12:46:12 +0900

    Feature/add checkbox component (#8)
    * fix requirements.md
    
    * fix Basic Tutorial
    
    * fix FetchApi & CheckBox
    
    * Complete implementation of CheckBoxPrefectures component

---
+++ src/components/CheckBoxPrefectures.vue
@@ -0,0 +1,88 @@
+<!-- AsyncSetupは使いずらく、口数がかかるので普通のスクリプトにしてマウントされたら実行 -->
+
+<script>
+import FetchApi from './FetchApi.vue'
+
+// App.vueでメソッドを実行するためにexport
+export default {
+  data() {
+    return {
+      prefectures: []
+    }
+  },
+  // マウントされたら実行
+  mounted() {
+    this.init()
+  },
+  methods: {
+    // チェックボックスの状態を変更する
+    onCheckBox(id, name, isChecked) {
+      // 都道府県リストのis_checkedはデフォでfalseなので反転させる
+      if (isChecked) {
+        this.prefectures[id - 1].isChecked = false
+      } else {
+        this.prefectures[id - 1].isChecked = true
+      }
+      console.log(
+        name + ' ID:' + id + ' のチェックボックスが' + this.prefectures[id - 1].isChecked + 'に変更されました'
+      )
+    },
+    // 初期表示
+    init: async function () {
+      try {
+        // APIから都道府県データを取得する
+        const apiKey = FetchApi.methods.ReadApiKey()
+        // データをバインドしてある変数に格納する
+        // そのデータをもとにチェックボックスを作成する
+        this.prefectures = await FetchApi.methods.FetchPrefectures(apiKey)
+      } catch (error) {
+        console.error(error.message)
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="prefectures_area">
+    <div v-for="prefecture in prefectures" :key="prefecture.id" class="prefecture">
+      <label :for="prefecture.id">
+        <input
+          type="checkbox"
+          :id="prefecture.id"
+          :checked="prefecture.isChecked"
+          @click="onCheckBox(prefecture.id, prefecture.name, prefecture.isChecked)"
+        />
+        {{ prefecture.name }}
+      </label>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.prefectures {
+  font-size: 15px;
+}
+.prefectures_area {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+}
+/* カーソルが近づいたらポインターにする */
+label {
+  cursor: pointer;
+}
+/* 解像度が狭くなったら適用 */
+@media screen and (max-width: 425px) {
+  .prefectures_area {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+  .prefectures {
+    font-size: 13px;
+  }
+  label {
+    cursor: pointer;
+    font-size: small;
+  }
+}
+</style>
